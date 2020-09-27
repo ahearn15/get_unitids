eee920f6a87a280ca6f3935c940d523b2334ee56
ahearn15 <ahearn15@gmail.com>
2020-09-27 13:03:44 -0400

    push

--- setup.py
+++ setup.py
@@ -1,24 +1,34 @@
-
-from setuptools import setup, find_packages
-
-with open("README.md", "r") as readme_file:
-    readme = readme_file.read()
-
-requirements = ["ipython>=6", "nbformat>=4", "nbconvert>=5", "requests>=2"]
+from setuptools import setup
 
 setup(
-    name="notebookc",
-    version="0.0.1",
-    author="Adam Hearn",
-    author_email="adam.hearn@collegetransitions.com",
-    description="Matches institutions to retreive IPEDS UnitIds",
-    long_description=readme,
-    long_description_content_type="text/markdown",
-    url="https://github.com/get_unitids/homepage/",
-    packages=find_packages(),
-    install_requires=requirements,
-    classifiers=[
-        "Programming Language :: Python :: 3.7",
-        "License :: OSI Approved :: MIT License",
-    ],
+  name = 'get_unitids',         # How you named your package folder (MyLib)
+  packages = ['get_unitids'],   # Chose the same as "name"
+  version = '0.1',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'IPEDS',   # Give a short description about your library
+  author = 'Adam Hearn',                   # Type in your name
+  author_email = 'adam.hearn@collegetransitions.com',      # Type in your E-Mail
+  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/ahearn15/get_unitids/archive/v_01.tar.gz',    # I explain this later on
+  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          're',
+          'math',
+          'numpy',
+          'itertools',
+          'collections',
+          'nltk',
+          'textblob',
+          'pandas',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
 )
